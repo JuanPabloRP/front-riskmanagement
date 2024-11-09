20f13daff59597fefaa0e8bb48562cf4586bff43
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-11-09 01:02:15 -0500

    feat: Implement role management with CreateRole and ListRoles components, and update routing

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -11,6 +11,8 @@ import PrivateLayout from '../layouts/PrivateLayout';
 import PublicLayout from '../layouts/PublicLayout';
 import Home from '../pages/private/home/Home';
 import Playground from '../pages/playground/Playground';
+import CreateRole from '../pages/private/role/CreateRole';
+import ListRoles from '../pages/private/role/ListRoles';
 
 const AppRoutes = () => {
 	return (
@@ -20,6 +22,8 @@ const AppRoutes = () => {
 			{/* Rutas privadas de la aplicación, todas las que van despues de registrarse o iniciar sesión */}
 			<Route path={PATHS.private.home} element={<PrivateLayout />}>
 				<Route path="" element={<Home />} />
+				<Route path={PATHS.private.roles.create} element={<CreateRole />} />
+				<Route path={PATHS.private.roles.base} element={<ListRoles />} />
 			</Route>
 
 			{/* Ruta principal a la que se entra la primera vez que se entra a la página */}
